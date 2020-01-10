ab05c5f6fa6b2b1c71cbd932e5509612b01581c3
Vinicius Nogueira <vinicius.nogueira@unidesk.com.br>
2020-01-10 08:57:51 -0300

    TabbedPage com CarouselPage

--- AppTabbedCarouselPage/AppTabbedCarouselPage/App.xaml.cs
+++ AppTabbedCarouselPage/AppTabbedCarouselPage/App.xaml.cs
@@ -10,11 +10,16 @@ namespace AppTabbedCarouselPage
         {
             InitializeComponent();
 
-            //TabbedPage tb = new TabbedPage();
-            CarouselPage tb = new CarouselPage();
+            CarouselPage cs = new CarouselPage();
+            cs.Children.Add(new MainPage());
+            cs.Children.Add(new Page1());
+            cs.Children.Add(new Page2());
+
+            TabbedPage tb = new TabbedPage();            
             tb.Children.Add(new MainPage());
             tb.Children.Add(new Page1());
-            tb.Children.Add(new Page2());
+            tb.Children.Add(cs);
+            
             MainPage = tb;
         }
 
